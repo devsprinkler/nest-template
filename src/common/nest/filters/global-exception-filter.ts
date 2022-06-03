09095d60f5e7c74d0fb5f486d836d6e3875c1001
MincheolKwon <dev.sprinkler@gmail.com>
2022-06-03 13:49:11 +0900

    feat: apply global filter
    Signed-off-by: MincheolKwon <dev.sprinkler@gmail.com>

---
+++ src/common/nest/filters/global-exception-filter.ts
@@ -0,0 +1,48 @@
+import { logger } from '@src/common/util/logger/winston-logger';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+@Catch()
+export class GlobalExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request: Request = ctx.getRequest();
+    const error: HttpException | Error =
+      exception instanceof HttpException ? exception : <Error>exception;
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    logger.error(error);
+    try {
+      response.status(status).json({
+        result: false,
+        code: status,
+        message:
+          error instanceof HttpException &&
+          typeof error.getResponse() === 'object'
+            ? (<{ [key: string]: any }>error.getResponse()).message
+            : error.message,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+    } catch (err) {
+      response.status(500).json({
+        result: false,
+        code: 500,
+        message: 'something wrong when server is running',
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
-import helmet from 'helmet';
 import { LoggingService } from '@src/common/util/logger/logging.service';
 import { logger } from '@src/common/util/logger/winston-logger';
+import { GlobalExceptionsFilter } from '@src/common/nest/filters/global-exception-filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new LoggingService(),
   });
+  app.useGlobalFilters(new GlobalExceptionsFilter());
   app.use(helmet());
   await app.listen(3000, '0.0.0.0', () => {
     logger.info('Server listen on port 3000');
