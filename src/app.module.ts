ca9ecbf7ee228d84a6e9211702d68e907a763cc1
MincheolKwon <dev.sprinkler@gmail.com>
2022-06-22 10:17:00 +0900

    fix: import path
    Signed-off-by: MincheolKwon <dev.sprinkler@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from '@src/app.controller';
 import { DocumentModule } from '@src/api/document/document.module';
 import { LoggerMiddleware } from '@src/common/nest/middlewares/logger.middleware';
 import { UserModule } from '@src/api/user/user.module';
-import { DatabaseModule } from './common/database/database.module';
+import { DatabaseModule } from '@src/common/database/database.module';
 
 @Module({
   imports: [DatabaseModule, DocumentModule, UserModule],
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
 import { LoggingService } from '@src/common/util/logger/logging.service';
 import { logger } from '@src/common/util/logger/winston-logger';
 import { GlobalExceptionsFilter } from '@src/common/nest/filters/global-exception-filter';
-import { GlobalResponseInterceptor } from './common/nest/interceptor/global-response-interceptor';
-import helmet from 'helmet';
-import { ValidationPipe } from '@nestjs/common';
+import { GlobalResponseInterceptor } from '@src/common/nest/interceptor/global-response-interceptor';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -21,8 +23,9 @@ async function bootstrap() {
     }),
   );
   app.use(helmet());
-  await app.listen(3000, '0.0.0.0', () => {
-    logger.info('Server listen on port 3000');
+  await app.listen(port, '0.0.0.0', () => {
+    // 0.0.0.0 means, Server use IPv4 only
+    logger.info(`Server listen on port ${port}`);
   });
 }
 bootstrap();
